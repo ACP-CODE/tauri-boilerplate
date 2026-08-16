93126367edbdecacb49f7f0424b67280ae26c049
Jaki Chen <jakichen@live.cn>
2026-08-16 14:20:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Chosse a template for your new Project
+
+- Multiplatform
+- iOS
+- macOS
+- Windows
+- Android
+
+## Application
+
+- App
+- Document App
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite-plus";
+
+// 判断是否在 CI 环境（GitHub Actions、GitLab CI 等都会自动设置 CI=true）
+const isCI = process.env.CI === "true" || process.env.CI === "1" || !!process.env.CI;
+
+// https://viteplus.dev/config
+export default defineConfig({
+    staged: {
+        "*": "vp check --fix",
+    },
+    fmt: {
+        ignorePatterns: ["dist/*"],
+    },
+    lint: {
+        options: {
+            typeAware: true,
+            typeCheck: true,
+        },
+    },
+    check: {
+        fmt: isCI,
+        lint: true,
+    },
+    test: {
+        projects: ["apps/*", "packages/*"],
+    },
+});

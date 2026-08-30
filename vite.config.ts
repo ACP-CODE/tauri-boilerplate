937fa763b46d5e22cf9d5661c8d0745db9f86fd5
Jaki Chen <jakichen@live.cn>
2026-08-30 18:46:50 +0800

    Update vite.cofnig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite-plus";
 
-// 判断是否在 CI 环境（GitHub Actions、GitLab CI 等都会自动设置 CI=true）
-const isCI = process.env.CI === "true" || process.env.CI === "1" || !!process.env.CI;
-
 // https://viteplus.dev/config
 export default defineConfig({
     staged: {

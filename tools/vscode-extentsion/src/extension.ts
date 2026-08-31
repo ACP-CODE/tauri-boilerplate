1ac88db69090f17d7e4474b88d3058031b381420
Jaki Chen <jakichen@live.cn>
2026-08-31 17:56:19 +0800

    Update

--- tools/vscode-extentsion/src/extension.ts
+++ tools/vscode-extentsion/src/extension.ts
@@ -1,7 +1,13 @@
 import * as vscode from "vscode";
 
-export function activate(_context: vscode.ExtensionContext): void {
-    // context.subscriptions.push()
+export function activate(context: vscode.ExtensionContext): void {
+    const disposable = vscode.commands.registerCommand("extension.helloWorld", () => {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage("Hello World!");
+    });
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate(): void {}
--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,6 @@ export default defineConfig({
         lint: true,
     },
     test: {
-        projects: ["apps/*", "packages/*"],
+        projects: ["apps/*", "packages/*", "tools/*"],
     },
 });
